185a1bf1121e810b432deb6ca0cee69a1cbbd0ca
yhanli <yhanl@hotmail.com>
2021-06-28 17:53:42 +1200

    fix error

--- DataWarehouse/titleMortgage/urls.py
+++ DataWarehouse/titleMortgage/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 router = routers.DefaultRouter()
 
-router.register('api/title-mortgage', TitleMortgageViewSet, 'titleMortgage')
+# router.register('api/title-mortgage', TitleMortgageViewSet, 'titleMortgage')
 
 urlpatterns = router.urls
