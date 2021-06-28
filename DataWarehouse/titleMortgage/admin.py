93ec1ba78c7e50ffa9206c800d33ea6ffd7baf0d
yhanli <yhanl@hotmail.com>
2021-06-29 09:06:47 +1200

    added new search form

--- DataWarehouse/titleMortgage/admin.py
+++ DataWarehouse/titleMortgage/admin.py
@@ -1,21 +1,35 @@
 from django.contrib import admin
 from .models import *
-
+from django.forms import ModelForm, Form
+from django.forms import DateField, CharField, ChoiceField, TextInput
+from django_admin_search.admin import AdvancedSearchAdmin
 
 # Register your models here.
 admin.site.site_header = "Admin"
 admin.site.site_title = "Admin Portal"
 admin.site.index_title = "Welcome"
 
-class TitleMortgagePageAdmin(admin.ModelAdmin):
+
+class FormSearchMortgage(Form):
+    encumbrancees = CharField(required=False, widget=TextInput(
+        attrs={
+            'filter_field': 'encumbrancees',
+            'filter_method': '__icontains'
+        }
+    ))
+    instrument_type = CharField(required=False)
+    title_no = CharField(required=False)
+
+
+class TitleMortgagePageAdmin(AdvancedSearchAdmin):
     list_display = ['title_no', 'encumbrancees', 'instrument_lodged_datetime', 'instrument_type', 'memorial_text']
-    # list_filter = [
+
+    # search_fields = [
+    #     'instrument_type',
     #     'encumbrancees'
     # ]
-    search_fields = [
-        'instrument_type',
-        'encumbrancees'
-    ]
+    search_form = FormSearchMortgage
     # ordering = ['-instrument_lodged_datetime']
+
 
 admin.site.register(TitleMortgage, TitleMortgagePageAdmin)
