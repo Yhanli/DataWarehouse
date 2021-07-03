22c93e248cb599c06d30870e7eb418fc8cd8783d
yhanli <yhanl@hotmail.com>
2021-07-03 20:25:05 +1200

    fix timezone

--- DataWarehouse/userProfile/middleware.py
+++ DataWarehouse/userProfile/middleware.py
@@ -1,8 +1,7 @@
 from django.utils import timezone
+import pytz
 from .models import UserProfile
-from django.contrib.auth.models import User
 from datetime import datetime
-from django.http import HttpResponse
 from django.utils.deprecation import MiddlewareMixin
 
 MAX_REQUEST_URL = 500
@@ -16,13 +15,13 @@ class UpdateLastActivityMiddleware(MiddlewareMixin):
     def process_view(self, request, view_func, view_args, view_kwargs):
         if request.user.is_authenticated and 'jsi18n' not in request.path:
             userProfile, created = UserProfile.objects.get_or_create(user_id=request.user.id)
-            userProfile.last_request_time = datetime.now()
+            userProfile.last_request_time = timezone.now()
             last_last_text = userProfile.last_request_text.split('\n')
 
             if len(last_last_text) > MAX_REQUEST_URL:
                 last_last_text = last_last_text[-MAX_REQUEST_URL:]
             url_path = get_request_path(request.build_absolute_uri)
-            req_time = datetime.now().strftime("%Y/%M/%d %H:%m:%S")
+            req_time = timezone.now().strftime("%Y/%M/%d %H:%m:%S")
             line = f'{req_time} {request.method} {url_path}'
             last_last_text.append(line)
             userProfile.last_request_text = '\n'.join(last_last_text)
