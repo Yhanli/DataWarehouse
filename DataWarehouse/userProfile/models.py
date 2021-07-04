ba6303b24e4a13f798ea59cb59284d4d10e5a112
yhanli <yhanl@hotmail.com>
2021-07-04 19:14:05 +1200

    add migrations

--- DataWarehouse/userProfile/models.py
+++ DataWarehouse/userProfile/models.py
@@ -8,5 +8,5 @@ from datetime import datetime
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, unique=True, on_delete=models.CASCADE)
-    last_request_time = models.DateTimeField(default=datetime.now())
+    last_request_time = models.DateTimeField(blank=True, null=True)
     last_request_text = models.TextField(blank=True)
