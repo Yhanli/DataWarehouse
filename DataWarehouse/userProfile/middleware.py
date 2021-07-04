e1c3dc51d7db2c3ed5823932e664bc5bdd1057e7
yhanli <yhanl@hotmail.com>
2021-07-04 19:09:20 +1200

    add request ip

--- DataWarehouse/userProfile/middleware.py
+++ DataWarehouse/userProfile/middleware.py
@@ -3,8 +3,10 @@ import pytz
 from .models import UserProfile
 from datetime import datetime
 from django.utils.deprecation import MiddlewareMixin
+import json
+from DataWarehouse.parameter import ENV
 
-MAX_REQUEST_URL = 500
+MAX_REQUEST_URL = ENV.MAX_REQUEST_URL
 
 
 def get_request_path(string):
@@ -17,12 +19,11 @@ class UpdateLastActivityMiddleware(MiddlewareMixin):
             userProfile, created = UserProfile.objects.get_or_create(user_id=request.user.id)
             userProfile.last_request_time = timezone.now()
             last_last_text = userProfile.last_request_text.split('\n')
-
             if len(last_last_text) > MAX_REQUEST_URL:
                 last_last_text = last_last_text[-MAX_REQUEST_URL:]
             url_path = get_request_path(request.build_absolute_uri)
             req_time = timezone.now().strftime("%Y/%m/%d %H:%M:%S")
-            line = f'{req_time} {request.method} {url_path}'
+            line = f'{req_time} - {request.META.get("REMOTE_ADDR")} - {request.method} {url_path}'
             last_last_text.append(line)
             userProfile.last_request_text = '\n'.join(last_last_text)
             userProfile.save()
