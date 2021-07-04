b82798c77a9e5daf8e4440a147e2071b45868a11
yhanli <yhanl@hotmail.com>
2021-07-04 19:30:15 +1200

    fix get ip

--- DataWarehouse/userProfile/middleware.py
+++ DataWarehouse/userProfile/middleware.py
@@ -13,6 +13,15 @@ def get_request_path(string):
     return str(string).split("'")[-2]
 
 
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
 class UpdateLastActivityMiddleware(MiddlewareMixin):
     def process_view(self, request, view_func, view_args, view_kwargs):
         if request.user.is_authenticated and 'jsi18n' not in request.path:
@@ -23,7 +32,7 @@ class UpdateLastActivityMiddleware(MiddlewareMixin):
                 last_last_text = last_last_text[-MAX_REQUEST_URL:]
             url_path = get_request_path(request.build_absolute_uri)
             req_time = timezone.now().strftime("%Y/%m/%d %H:%M:%S")
-            line = f'{req_time} - {request.META.get("REMOTE_ADDR")} - {request.method} {url_path}'
+            line = f'{req_time} - {get_client_ip(request)} - {request.method} {url_path}'
             last_last_text.append(line)
             userProfile.last_request_text = '\n'.join(last_last_text)
             userProfile.save()
