de6960c580eeaaad01199a2d971cd92ae612ce8b
yhanli <yhanl@hotmail.com>
2021-09-04 20:57:44 +1200

    improve nginx config

--- DataWarehouse/entrypoint.sh
+++ DataWarehouse/entrypoint.sh
@@ -1,4 +1,4 @@
-pipenv run python manage.py migrate --no-input
-pipenv run python manage.py collectstatic --no-input
+pipenv run python manage.py migrate --noinput
+pipenv run python manage.py collectstatic --noinput
 
 pipenv run gunicorn --workers 5 DataWarehouse.wsgi:application --bind 0.0.0.0:8000
